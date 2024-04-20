9cc0680527d54d5dc50cad959feadeddaa7e97dc
Moatasem Sorour <83554187+Moto90@users.noreply.github.com>
2024-04-19 23:54:39 -0400

    Update course_details.php

--- course_details.php
+++ course_details.php
@@ -112,7 +112,7 @@ if ($page * $per_page >= $total_sentences) {
         <button onclick="location.href='blog.php'" class="button">Blogs</button>
         <button onclick="location.href='track_progress.php'" class="button">Dashboard</button>
 	<button onclick="location.href='search.php'" class="button">Search</button>
-        <button onclick="location.href='about_us.php'">About Us</button>
+       	<button onclick="location.href='about_us.php'"class="button">About Us</button>
         <button onclick="location.href='contact.php'" class="button">Contact Us</button>
     </div>
 
