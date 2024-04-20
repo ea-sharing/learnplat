179b73c83ac046df69a6f63a37218741225afe2b
Moatasem Sorour <83554187+Moto90@users.noreply.github.com>
2024-04-19 23:54:18 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -77,8 +77,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <button onclick="location.href='assessments.php'" class="button">Assessments</button>
         <button onclick="location.href='blog.php'" class="button">Blogs</button>
         <button onclick="location.href='track_progress.php'" class="button">Dashboard</button>
-	    <button onclick="location.href='search.php'" class="button">Search</button>
-        <button onclick="location.href='about_us.php'">About Us</button>
+    	<button onclick="location.href='search.php'" class="button">Search</button>
+        <button onclick="location.href='about_us.php'"class="button">About Us</button>
         <button onclick="location.href='contact.php'" class="button">Contact Us</button>
         
     </div>
